b660718be6f3dddda901171e36968068c57cd3de
BekoSm <b.smagulov.1999@gmail.com>
2022-07-21 18:49:05 +0600

    Push#2

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -2,8 +2,8 @@ package school.mjc.stage0.loops.task2;
 
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
-        int count = 0;
-        int result = 0;
+        int count = 1;
+        int result = 1;
         while (count != multiplyByAndToInclusive) {
             result = count * result;
             count++;
--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -2,13 +2,17 @@ package school.mjc.stage0.loops.task2;
 
 public class PowerOfTwo {
     public void printPower(int power) {
+        double result = 1;
         int count = 0;
+        String word = "";
         if (power<0){
             System.out.println("too much power");
         }
         else {
-            while (count != power) {
-                System.out.println(Math.pow(2, count));
+            while(count != power+1) {
+                result = Math.pow(2, count);
+                word = String.valueOf((int)result);
+                System.out.println(word);
                 count++;
             }
         }
