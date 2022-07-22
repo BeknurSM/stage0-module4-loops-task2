85c3b770234bd2bf1d3e3970ef4c76345589ff69
BekoSm <b.smagulov.1999@gmail.com>
2022-07-22 12:02:38 +0600

    Push#6

--- src/main/java/school/mjc/stage0/loops/task2/EvenNumbersPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task2/EvenNumbersPrinter.java
@@ -2,15 +2,15 @@ package school.mjc.stage0.loops.task2;
 
 public class EvenNumbersPrinter {
     public void printEvenNumbers(int printTillInclusive) {
-        int count = 0;
+        int count = 2;
         if (printTillInclusive == 0){
             System.out.print("");
         }
         else
         {
-        while (count != printTillInclusive) {
-            count = count + 2;
+        while (count <= printTillInclusive) {
             System.out.println(count);
+            count = count + 2;
         }
         }
     }
